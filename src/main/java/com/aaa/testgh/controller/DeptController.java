ddd53e0b397e9b3d303afe8ce79d066d34420b7b
hlq <1242085651@qq.com>
2018-11-29 10:43:44 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:hulu
+ * createTime:2018-11-29 10:14
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+    }
+}
